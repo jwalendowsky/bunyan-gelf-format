289f4e35869685164bc220fcb092f5a57a4841c4
Jorge Walendowsky Filho <jorge.filho@wal-mart.com>
2016-06-02 17:44:29 -0300

    changed the version to not use classes

--- test/core.js
+++ test/core.js
@@ -83,4 +83,119 @@ describe("core", () => {
 
     logger.debug({additional : 'additional field'}, 'testing core log debug capabilities.');
   });
+
+
+
+  it ('should log an error and the stack must be treated.', done => {
+    let stream = bunyanGelfFormat.createStream({raw : true}),
+        logger = bunyan.createLogger(
+            {name : 'core-logger', streams : [{type: 'raw', stream : stream}]}),
+        errorThatHappened = null;
+
+    stream.pipe(new Writable({
+      objectMode: true,
+      write : function (message, encoding, callback) {
+        callback(null, message);
+
+        expect(message.level).to.equal(bunyanGelfFormat.LOG_LEVELS.ERROR);
+        expect(message.short_message).to.equal('Failed to get the resources.');
+        expect(message.facility).to.equal('core-logger');
+        expect(message.full_message).to.equal(errorThatHappened.stack);
+        expect(message._line).to.equal(118);
+
+        done();
+      }
+    }));
+
+    try {
+      functionThatThrowsAnError();
+    } catch (error) {
+      errorThatHappened = error;
+      logger.error(error, 'Failed to get the resources.');
+    }
+
+    function functionThatThrowsAnError() {
+      throw new Error('This error happenned within the functionThatThrowsAnError.');
+    }
+  });
+
+
+  it ('should log an error with no log message and the stack must be treated.', done => {
+    let stream = bunyanGelfFormat.createStream({raw : true}),
+        logger = bunyan.createLogger(
+            {name : 'core-logger', streams : [{type: 'raw', stream : stream}]}),
+        errorThatHappened = null;
+
+    stream.pipe(new Writable({
+      objectMode: true,
+      write : function (message, encoding, callback) {
+        callback(null, message);
+
+        expect(message.level).to.equal(bunyanGelfFormat.LOG_LEVELS.ERROR);
+        expect(message.short_message).to.equal('This error happenned within the functionThatThrowsAnotherError.');
+        expect(message.facility).to.equal('core-logger');
+        expect(message.full_message).to.equal(errorThatHappened.stack);
+        expect(message._line).to.equal(152);
+
+        done();
+      }
+    }));
+
+    try {
+      functionThatThrowsAnotherError();
+    } catch (error) {
+      errorThatHappened = error;
+      logger.error(error);
+    }
+
+    function functionThatThrowsAnotherError() {
+      throw new Error('This error happenned within the functionThatThrowsAnotherError.');
+    }
+  });
+
+  it ('should log a message and it must be a string, since raw is false.', done => {
+    let stream = bunyanGelfFormat.createStream({raw : false}),
+        logger = bunyan.createLogger(
+            {name : 'core-logger', streams : [{type: 'raw', stream : stream}]}),
+        errorThatHappened = null;
+
+    stream.pipe(new Writable({
+      objectMode: true,
+      write : function (message, encoding, callback) {
+        console.log(message);
+        expect(typeof message).to.equal('string');
+        expect(message.indexOf('This is a message.')).to.be.above(-1);
+        done();
+      }
+    }));
+
+    logger.info('This is a message.');
+  });
+
+  it ('should log more than one message.', done => {
+    let stream = bunyanGelfFormat.createStream({raw : false}),
+        logger = bunyan.createLogger(
+            {name : 'core-logger', streams : [{type: 'raw', stream : stream}]}),
+        errorThatHappened = null,
+        messagesCount = 0;
+
+    stream.pipe(new Writable({
+      objectMode: true,
+      write : function (message, encoding, callback) {
+        messagesCount++;
+
+        if (messagesCount == 5) {
+          done();
+        }
+
+        callback(null, message);
+      }
+    }));
+
+    logger.info('This is a message.');
+    logger.info('This is another message.');
+    logger.info('This is one more message.');
+    logger.info('This is an additional message.');
+    logger.info('This is the final message.');
+  });
 });
